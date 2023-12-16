edeff4fda40ede74d5f57211afbfac184a559f1f
noaa <noaamaman325158@gmail.com>
2023-12-16 13:37:22 +0200

    Here I add option to identify also a person and save snapshot for each recognition. meanwhile there is tem directory that store all the relevant snapshots.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import cv2
 import numpy as np
 import datetime
+import os
+
+snapshot_dir = r"/home/noaa/PycharmProjects/ObjectDetection/snapshot_directory"  # Replace with your desired path
+os.makedirs(snapshot_dir, exist_ok=True)
 
 # Load pre-trained model and classes
 net = cv2.dnn.readNetFromCaffe('deploy.prototxt', 'mobilenet_iter_73000.caffemodel')
 classes = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow",
            "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 
-def detect_dogs(frame):
+
+def detect_dogs_and_people(frame):
     (h, w) = frame.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
 
@@ -19,28 +24,33 @@ def detect_dogs(frame):
         confidence = detections[0, 0, i, 2]
         if confidence > 0.2:  # Confidence threshold
             idx = int(detections[0, 0, i, 1])
-            if classes[idx] != "dog":
+            if classes[idx] not in ["dog", "person"]:
                 continue
+            detected_class = classes[idx]
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (startX, startY, endX, endY) = box.astype("int")
             cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
-            label = "{}: {:.2f}%".format(classes[idx], confidence * 100)
+            label = "{}: {:.2f}%".format(detected_class, confidence * 100)
             cv2.putText(frame, label, (startX, startY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-            # Write detection time to file
+            # Write detection time to file and take a snapshot
+            snapshot_filename = f"{snapshot_dir}/snapshot_{detected_class}_{current_time.strftime('%Y%m%d_%H%M%S')}.png"
+            cv2.imwrite(snapshot_filename, frame)  # Save the current frame as an image
             with open("detections.txt", "a") as file:
-                file.write(f"Dog detected at {current_time.strftime('%Y-%m-%d %H:%M:%S')}\n")
-            break  # Stop after the first detection to avoid multiple logs for the same dog in one frame
+                file.write(
+                    f"{detected_class.capitalize()} detected at {current_time.strftime('%Y-%m-%d %H:%M:%S')}, snapshot saved as {snapshot_filename}\n")
+            break  # Stop after the first detection to avoid multiple logs and snapshots for the same object in one frame
+
 
 # Load a video
 cap = cv2.VideoCapture('dog_poo.mp4')
-
 while True:
     ret, frame = cap.read()
     if not ret:
         break
-    detect_dogs(frame)
-    cv2.imshow('Frame', frame)
+    resized_frame = cv2.resize(frame, (640, 480))
+    detect_dogs_and_people(resized_frame)
+    cv2.imshow('Frame', resized_frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
---
+++ test_live_connection.py
@@ -0,0 +1,2 @@
+import cv2
+cap = cv2.VideoCapture('https://192.168.1.45:8080')
